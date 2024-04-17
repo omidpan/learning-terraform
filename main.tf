b07d48abcd37076804b3ab9bd89629b25520aa89
Omid Panahi <omidpnteaching@gmail.comgit config --global user.name leo>
2024-04-17 11:17:35 -0400

    add http traffics

--- main.tf
+++ main.tf
@@ -39,6 +39,15 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
 resource "aws_security_group_rule" "blog_everything_out" {
   type           = "egress"
   from_port      = 0
