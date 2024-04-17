e74a1149fa752eaa1239fc2e47cb3bdd95396c0c
Omid Panahi <omidpnteaching@gmail.comgit config --global user.name leo>
2024-04-17 10:55:45 -0400

    adding security groups

--- main.tf
+++ main.tf
@@ -10,15 +10,40 @@ data "aws_ami" "app_ami" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
+  
   owners = ["979382823631"] # Bitnami
 }
-
+data "aws_vpc" "default"{
+  default      =true
+}
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  vpc_security_group_ids =[aws_security_group.blog.id]
   tags = {
-    Name = "HelloWorld"
+    Name        = "Learning Terraform"
   }
+}
+
+resource "aws_security_group" "blog"{
+  name           = "blog"
+  description    = "Allow http and https in . Allow everthing out"
+  vpc_id         = data.aws_vpc.default.id
+}
+resource "aws_security_group_rule" "blog_https_in" {
+  type           = "ingress"
+  from_port      = 443
+  to_port        = 443
+  protocol       = "tcp"
+  cidr_block     = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type           = "egress"
+  from_port      = 0
+  to_port        = 0
+  protocol       = "-1"
+  cidr_block     = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
