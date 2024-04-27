e23248fbebd8eb3f91e22bbc235599453cbc1fa4
Omidpn <omidpnteaching@gmail.com>
2024-04-26 21:54:21 -0400

    remove resources test

--- main.tf
+++ main.tf
@@ -16,24 +16,24 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default"{
   default      =true
 }
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids =[module.blog_sg.security_group_id]
-  tags = {
-    Name        = "Learning Terraform"
-  }
-}
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-  name    = "blog"
+# resource "aws_instance" "blog" {
+#   ami           = data.aws_ami.app_ami.id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids =[module.blog_sg.security_group_id]
+#   tags = {
+#     Name        = "Learning Terraform"
+#   }
+# }
+# module "blog_sg" {
+#   source  = "terraform-aws-modules/security-group/aws"
+#   version = "5.1.2"
+#   name    = "blog"
 
-  vpc_id         = data.aws_vpc.default.id
+#   vpc_id         = data.aws_vpc.default.id
 
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+#   ingress_rules       = ["http-80-tcp","https-443-tcp"]
+#   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
+#   egress_rules = ["all-all"]
+#   egress_cidr_blocks = ["0.0.0.0/0"]
+# }
