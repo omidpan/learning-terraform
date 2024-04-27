dfc8c04345213f32a48e475acb506afe4d28118d
Omidpn <omidpnteaching@gmail.com>
2024-04-26 22:03:52 -0400

    create resources before run

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
-# data "aws_ami" "app_ami" {
-#   most_recent = true
+data "aws_ami" "app_ami" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-#   }
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
   
-#   owners = ["979382823631"] # Bitnami
-# }
-# data "aws_vpc" "default"{
-#   default      =true
-# }
-# resource "aws_instance" "blog" {
-#   ami           = data.aws_ami.app_ami.id
-#   instance_type = var.instance_type
-#   vpc_security_group_ids =[module.blog_sg.security_group_id]
-#   tags = {
-#     Name        = "Learning Terraform"
-#   }
-# }
-# module "blog_sg" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "5.1.2"
-#   name    = "blog"
+  owners = ["979382823631"] # Bitnami
+}
+data "aws_vpc" "default"{
+  default      =true
+}
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids =[module.blog_sg.security_group_id]
+  tags = {
+    Name        = "Learning Terraform"
+  }
+}
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name    = "blog"
 
-#   vpc_id         = data.aws_vpc.default.id
+  vpc_id         = data.aws_vpc.default.id
 
-#   ingress_rules       = ["http-80-tcp","https-443-tcp"]
-#   ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-#   egress_rules = ["all-all"]
-#   egress_cidr_blocks = ["0.0.0.0/0"]
-# }
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-# output "instance_ami" {
-#   value = aws_instance.blog.ami
-# }
+output "instance_ami" {
+  value = aws_instance.blog.ami
+}
 
-# output "instance_arn" {
-#  value = aws_instance.blog.arn
+output "instance_arn" {
+ value = aws_instance.blog.arn
 
-# }
+}
