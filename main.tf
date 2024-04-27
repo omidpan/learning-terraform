1d9449d657352528cde653df17a845a541c4f84f
Omidpn <omidpnteaching@gmail.com>
2024-04-26 21:58:33 -0400

    destroy all

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+# data "aws_ami" "app_ami" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
   
-  owners = ["979382823631"] # Bitnami
-}
-data "aws_vpc" "default"{
-  default      =true
-}
+#   owners = ["979382823631"] # Bitnami
+# }
+# data "aws_vpc" "default"{
+#   default      =true
+# }
 # resource "aws_instance" "blog" {
 #   ami           = data.aws_ami.app_ami.id
 #   instance_type = var.instance_type
