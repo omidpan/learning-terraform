437f2d67fe9e80ab9135492c532453e455768bde
Omidpn <omidpnteaching@gmail.com>
2024-05-13 16:47:59 -0400

    change subnet

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids =[module.blog_sg.security_group_id]
-  subnet_id = module.blug_vpc.public_subnets[0]
+  subnet_id = module.blug_vpc.public_subnets[1]
   tags = {
     Name        = "101-aws"
   }
