4e081808c64745e0b984eecdae4d7fbf6002e4f6
Omidpn <omidpnteaching@gmail.com>
2024-05-13 17:47:33 -0400

    remove resouce instance

--- main.tf
+++ main.tf
@@ -30,15 +30,15 @@ module "blug_vpc" {
     Environment = "dev"
   }
 }
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids =module.blog_sg.security_group_id
-  subnet_id = module.blug_vpc.public_subnets[0]
-  tags = {
-    Name        = "101-aws"
-  }
-}
+# resource "aws_instance" "blog" {
+#   ami           = data.aws_ami.app_ami.id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids =module.blog_sg.security_group_id
+#   subnet_id = module.blug_vpc.public_subnets[0]
+#   tags = {
+#     Name        = "101-aws"
+#   }
+# }
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
