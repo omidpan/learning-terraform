37fccfb6b2b936f12e46fd5fa111d27c33915f9e
Omidpn <omidpnteaching@gmail.com>
2024-05-13 17:42:31 -0400

    autoscale

--- main.tf
+++ main.tf
@@ -39,6 +39,21 @@ resource "aws_instance" "blog" {
     Name        = "101-aws"
   }
 }
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+ # Autoscaling group
+  name = "blog"
+
+  min_size                  = 1
+  max_size                  = 2
+  vpc_zone_identifier       = module.blug_vpc.public_subnets
+  target_group_arns         =module.blog_alb.target_group_arns
+  security_groups           =[module.blog_sg.security_group_id]
+
+  image_id  = data.aws_ami.app_ami
+  instance_type = var.instance_type
+}
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
