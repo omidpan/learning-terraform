7536a2442eca96cfbf21c23528755443dd89d466
Omidpn <omidpnteaching@gmail.com>
2024-05-13 17:12:15 -0400

    add load balancer

--- main.tf
+++ main.tf
@@ -34,9 +34,53 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids =[module.blog_sg.security_group_id]
-  subnet_id = module.blug_vpc.public_subnets[1]
+  subnet_id = module.blug_vpc.public_subnets[0]
   tags = {
     Name        = "101-aws"
+  }
+}
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blug_vpc.vpc_id
+  subnets = module.blug_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "101-aws"
   }
 }
 module "blog_sg" {
