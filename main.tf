853217f35b85e694d364f43d39e1e0da8cb41121
Omidpn <omidpnteaching@gmail.com>
2024-05-13 17:49:21 -0400

    get code from branch 04_02

--- main.tf
+++ main.tf
@@ -30,70 +30,54 @@ module "blug_vpc" {
     Environment = "dev"
   }
 }
-# resource "aws_instance" "blog" {
-#   ami           = data.aws_ami.app_ami.id
-#   instance_type = var.instance_type
-#   vpc_security_group_ids =module.blog_sg.security_group_id
-#   subnet_id = module.blug_vpc.public_subnets[0]
-#   tags = {
-#     Name        = "101-aws"
-#   }
-# }
-module "autoscaling" {
+
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
- # Autoscaling group
+
   name = "blog"
 
-  min_size                  = 1
-  max_size                  = 2
-  vpc_zone_identifier       = module.blug_vpc.public_subnets
-  target_group_arns         =module.blog_alb.target_group_arns
-  security_groups           =[module.blog_sg.security_group_id]
-
-  image_id  = data.aws_ami.app_ami
-  instance_type = var.instance_type
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
+
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
-  name    = "blog-alb"
+
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
-  vpc_id  = module.blug_vpc.vpc_id
-  subnets = module.blug_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
   target_groups = [
-      {
-        name_prefix      = "blog-"
-        backend_protocol = "HTTP"
-        backend_port     = 80
-        target_type      = "instance"
-        # targets = [
-        #   {
-        #     target_id = aws_instance.blog.id
-        #     port = 80
-        #   },
-        #   {
-        #     target_id = aws_instance.blog.id
-        #     port = 8080
-        #   }
-        # ]
-      }
-    ]
-
-    http_tcp_listeners = [
-      {
-        port               = 80
-        protocol           = "HTTP"
-        target_group_index = 0
-      }
-    ]
-
-    tags = {
-      Environment = "dev"
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
     }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
 }
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
