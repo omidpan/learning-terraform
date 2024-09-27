b3e43434c9498431064d8cfe7fdfa7a890a1b520
Omidpn <omidpnteaching@gmail.com>
2024-09-27 15:33:41 -0400

    remove vpc check passed

--- arch/main.tf
+++ arch/main.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
+# resource "aws_vpc" "vpc" {
+#     cidr_block = "10.0.0.0/16"
+#     instance_tenancy = "default"
 
-    tags = {
-      name = "my-arch-vpc"
-    }
+#     tags = {
+#       name = "my-arch-vpc"
+#     }
   
-}
+# }
