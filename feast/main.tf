8b1e3105bcc01b5c7be51a92177b2cc82733e6d8
Omidpn <omidpnteaching@gmail.com>
2024-09-30 15:54:15 -0400

    remove assumerole

--- feast/main.tf
+++ feast/main.tf
@@ -22,24 +22,24 @@ resource "aws_s3_bucket_object" "loan_features_file_upload" {
   source = "${path.module}/../data/loan_table.parquet"
 }
 
-resource "aws_iam_role" "s3_spectrum_role" {
-  name = "s3_spectrum_role"
+# resource "aws_iam_role" "s3_spectrum_role" {
+#   name = "s3_spectrum_role"
 
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "redshift.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Principal": {
+#                 "Service": "redshift.amazonaws.com"
+#             },
+#             "Action": "sts:AssumeRole"
+#         }
+#     ]
+# }
+# EOF
+# }
 
 data "aws_iam_role" "AWSServiceRoleForRedshift" {
   name = "AWSServiceRoleForRedshift"
@@ -55,29 +55,29 @@ resource "aws_iam_role_policy_attachment" "glue_full" {
   role       = aws_iam_role.s3_spectrum_role.name
 }
 
-resource "aws_iam_policy" "s3_full_access_policy" {
-  name = "s3_full_access_policy"
+# resource "aws_iam_policy" "s3_full_access_policy" {
+#   name = "s3_full_access_policy"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "s3:*"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role_policy_attachment" "s3-policy-attachment" {
-  role       = aws_iam_role.s3_spectrum_role.name
-  policy_arn = aws_iam_policy.s3_full_access_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "s3-policy-attachment" {
+#   role       = aws_iam_role.s3_spectrum_role.name
+#   policy_arn = aws_iam_policy.s3_full_access_policy.arn
+# }
 
 resource "aws_redshift_cluster" "feast_redshift_cluster" {
   cluster_identifier = "${var.project_name}-redshift-cluster"
